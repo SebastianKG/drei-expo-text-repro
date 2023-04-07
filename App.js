65a8f7ef21f8ec280041dada16c2881069094ae1
Sebastian Kazenbroot-Guppy <sebastian.kg@gmail.com>
2023-04-08 00:47:38 +0100

    Minimal repro of Text problems

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { Canvas } from "@react-three/fiber";
+import { Text } from "@react-three/drei";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ width: "100%", height: "100%" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh position-y={0.75} rotation-x={-Math.PI / 6}>
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <Text position-y={-0.75} rotation-x={-Math.PI / 6}>
+          Hello World
+        </Text>
+      </Canvas>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
